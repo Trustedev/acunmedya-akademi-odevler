0365c97397874d6fa5ce2e0963ac44a1c2ab2005
Baran Uygur <52936700+Trustedev@users.noreply.github.com>
2023-08-18 23:42:03 +0300

    Add files via upload

---
+++ Odev_130823/Odev_130823_8/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] myList = new int[5]; 
+
+for(int i = 0; i < myList.Length; i++){
+    Console.WriteLine((i+1) + "/" + myList.Length + " sayı girişi yapın: ");
+    myList[i] = int.Parse(Console.ReadLine());
+}
+int numMax = myList[0], numMin = myList[0];
+for (int i = 0; i < myList.Length; i++){
+    if (myList[i] > numMax)
+    {
+        numMax = myList[i];
+    }
+}
+for (int i = 0; i < myList.Length; i++){
+    if (myList[i] < numMin){
+        numMin = myList[i];
+    }
+}
+Console.WriteLine("Listedeki en küçük sayı: " + numMin + "\nListedeki en büyük sayı: " + numMax);
+
