8c8b3a46e8bdd15d8b77572902929ec2f5c6c693
Baran Uygur <52936700+Trustedev@users.noreply.github.com>
2023-08-18 22:58:04 +0300

    Add files via upload

---
+++ Odev_130823/Odev_130823_6/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] myList = new int[10] {5, 2, 5, 3, 1, 9, 9, 6, 5, 2};
+int[] pureList = new int[10];
+int counter = 0; //counts the amount of numbers in purelist
+foreach (int i in myList){
+    if(!pureList.Contains(i)){        
+        pureList[counter] = i;
+        counter++;
+    }}
+Console.WriteLine("Orjinal liste: ");
+for (int i = 0; i < myList.Length; i++){   
+    Console.Write(myList[i] + " ");
+}
+Console.WriteLine("\n Düzenlenmiş liste: ");
+for (int i = 0; i < counter; i++){    
+Console.Write(pureList[i] + " ");
+}
