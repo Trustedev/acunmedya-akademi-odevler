3df0bada88078215afa461e1d05ffccbd445108c
Baran Uygur <52936700+Trustedev@users.noreply.github.com>
2023-08-18 23:15:02 +0300

    Add files via upload

---
+++ Odev_130823/Odev_130823_7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+static bool IsPrime(int num) { 
+if (num <= 1)
+    return false;
+
+if (num <= 3)
+    return true;
+
+if (num % 2 == 0 || num % 3 == 0)
+
+    return false;
+for (int i = 5; i * i <= num; i += 6)
+{
+    if (num % i == 0 || num % (i + 2) == 0)
+        return false;
+}
+return true;
+}
+
+int[] myList = new int[10] {1, 2, 3, 4, 5, 6, 7, 8, 9, 13};
+int sumPrime = 0;
+
+foreach (int i in myList){
+    if(IsPrime(i)){
+        sumPrime += i;
+    }}
+
+Console.WriteLine("Sayı listesi: ");
+for (int i = 0; i < myList.Length; i++)
+{
+    Console.Write(myList[i] + " ");
+}
+Console.WriteLine("\nİçindeki asal sayıların toplamı: " + sumPrime);
