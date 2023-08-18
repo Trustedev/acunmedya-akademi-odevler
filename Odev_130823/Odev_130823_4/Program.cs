2bb59d5d542187e6ab6bdd83f7c1422665e2b0cd
Baran Uygur <52936700+Trustedev@users.noreply.github.com>
2023-08-18 20:05:09 +0300

    Add files via upload

---
+++ Odev_130823/Odev_130823_4/Program.cs
@@ -0,0 +1,28 @@
+﻿static bool IsPerfectNum(int number)
+{
+    int sum = 0;
+    for (int i = 1; i < number; i++)
+    {
+        if (number % i == 0)
+        {
+            sum += i;
+        }
+    }
+    return sum == number;
+}
+
+for (int number = 1; number <= 1000; number++)
+{
+    if (IsPerfectNum(number))
+    {
+        Console.Write(number + " mükemmel sayıdır, çarpanları: ");
+        for (int i = 1; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
